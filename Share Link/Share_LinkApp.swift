0e70ddbd748463fdd05aecb2825aad522b310829
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-23 15:29:24 +0300

    Initial Commit

---
+++ Share Link/Share_LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_LinkApp.swift
+//  Share Link
+//
+//  Created by Leonard Mutugi on 23/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Share_LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
