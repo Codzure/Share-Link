880449e92a3d92372e02cc5f87d8ca60f14f74b9
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-23 15:44:45 +0300

    first commit

--- Share Link/ContentView.swift
+++ Share Link/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let url = "https://www.linkedin.com/in/leonardmutugi/"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("LinkedIn profile!")
+                .font(.headline)
+            ShareLink(item: url)
+                .padding()
+            Label("Leonard's LinkedIn", systemImage: "heart.fill")
+                .foregroundColor(.orange)
         }
         .padding()
     }
